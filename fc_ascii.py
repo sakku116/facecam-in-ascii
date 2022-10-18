787cccffa265c103a24e12679df8f265749514f2
Zakky <88846365+sakku116@users.noreply.github.com>
2022-10-18 21:06:00 +0700

    .

--- fc_ascii.py
+++ fc_ascii.py
@@ -10,11 +10,15 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     # save image
-    cv2.imwrite("main.jpg", frame)
+    cv2.imwrite("main.jpg", rgb_frame)
 
     # convert img to txt
     pywhatkit.image_to_ascii_art("./main.jpg", "main_cvt")
 
     # print text file content
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print(open("./main_cvt.txt", "r").read())
+    with open("./main_cvt.txt", "r") as f:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f.read())
+
+    cv2.imshow('Eye Controlled Mouse', frame)
+    cv2.waitKey(1)
